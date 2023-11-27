3492e43fc237f450eb433b875f262939e7c0df45
cattyburn <87564082+aishaaurora@users.noreply.github.com>
2023-11-27 22:03:21 +0700

    baru

--- box.cpp
+++ box.cpp
@@ -1,199 +1,186 @@
-/////////////////////////////////          
-// box.cpp
-//
-// This program draws a wire box.
-//
-// Sumanta Guha.
-/////////////////////////////////
+//Adyatma Dyma
+//Aisha Aurora 
 
 #include <iostream>
-
 #include <GL/glew.h>
-#include <GL/freeglut.h> 
+#include <GL/freeglut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-
 // Drawing routine.
 
 float rotationAngle = 0.0f;
-float rotationSpeed = 0.0f; // Inisialisasi kecepatan rotasi
+float rotationSpeed = 0.0f;
 int prevX = 0, prevY = 0;
 bool mouseRotate = false;
 
 void drawFrameKanan(void)
 {
-	glPushMatrix();
-	glColor3f(0.28, 0.16, 0.14);
-	glTranslatef(5.1, -60.0, -20.0);
-	glRotatef(rotationAngle, 0.0, 1.0, 0.0); // Terapkan rotasi.
-	glScalef(0.1, 55.0, 1.0);
-	GLfloat material_diffuse[] = { 0.7, 0.7, 0.7, 1.0 };
-	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse);
-	glutSolidCube(5.0); // Box.
-	glPopMatrix();
-
+    glPushMatrix();
+    glColor3f(0.28, 0.16, 0.14);
+    glTranslatef(2.5, -70.0, 0.0);
+    //glRotatef(rotationAngle, 0.0, 1.0, 0.0); // Terapkan rotasi.
+    glScalef(0.025, 30.0, 1.0);
+    GLfloat material_diffuse[] = { 0.7, 0.7, 0.7, 1.0 };
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse);
+    glutSolidCube(5.0); // Box.
+    glPopMatrix();
 }
 
 void drawFrameKiri(void)
 {
-	glColor3f(0.28, 0.16, 0.14);
-	glTranslatef(-128.0, 0.0, -20.0);
-	glRotatef(rotationAngle, 0.0, 1.0, 0.0); // Terapkan rotasi.
-	glScalef(1.5, 1.45, 2.0);
-	GLfloat material_diffuse[] = { 0.7, 0.7, 0.7, 1.0 };
-	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse);
-	glutSolidCube(5.0); // Box.
+    glPushMatrix();
+    glColor3f(0.28, 0.16, 0.14);
+    glTranslatef(-2.5, -70.0, 0.0);
+    //glRotatef(rotationAngle, 0.0, 1.0, 0.0); // Terapkan rotasi.
+    glScalef(0.025, 30.0, 1.0);
+    GLfloat material_diffuse[] = { 0.7, 0.7, 0.7, 1.0 };
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse);
+    glutSolidCube(5.0); // Box.
+    glPopMatrix();
 }
 
 void drawFrameBlkg(void)
 {
-	glColor3f(0.22, 0.16, 0.14);
-	glTranslatef(52.0, 0.0, -24.0);
-	glRotatef(rotationAngle, 0.0, 1.0, 0.0); // Terapkan rotasi.
-	glScalef(30.0, 1.45, 2.0);
-	GLfloat material_diffuse[] = { 0.7, 0.7, 0.7, 1.0 };
-	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse);
-	glutSolidCube(5.0); // Box.
+    glPushMatrix();
+    glColor3f(0.22, 0.16, 0.14);
+    glTranslatef(0.0, -70.0, -2.5);
+    //glRotatef(rotationAngle, 0.0, 1.0, 0.0); // Terapkan rotasi.
+    glScalef(1.0, 30.0, 0.01);
+    GLfloat material_diffuse[] = { 0.7, 0.7, 0.7, 1.0 };
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse);
+    glutSolidCube(5.0); // Box.
+    glPopMatrix();
 }
 
 void drawTumpukanRak(void)
 {
-	glColor3f(0.18, 0.16, 0.14);
-	// Modeling transformations.
-	glTranslatef(0.0, -3.0, -20.0);
-	glRotatef(rotationAngle, 0.0, 1.0, 0.0); // Terapkan rotasi.
-	glScalef(3.0, 0.05, 1.0);
-	GLfloat material_diffuse[] = { 0.7, 0.7, 0.7, 1.0 };
-	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse);
-	glutSolidCube(5.0); // Box.
+    glColor3f(0.18, 0.16, 0.14);
+    // Modeling transformations.
+    glTranslatef(0.0, -3.0, -20.0);
+    glRotatef(rotationAngle, 0.0, 1.0, 0.0); // Terapkan rotasi.
+    glScalef(3.0, 0.05, 1.0);
+    GLfloat material_diffuse[] = { 0.7, 0.7, 0.7, 1.0 };
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse);
+    glutSolidCube(5.0); // Box.
 
-	for (int i = 0; i < 3; i++) {
-		glTranslatef(0.0, 40.0, 0.0);
-		glutSolidCube(5.0); // Kubus tambahan.
-	}
-
+    for (int i = 0; i < 3; i++) {
+        glTranslatef(0.0, 40.0, 0.0);
+        glutSolidCube(5.0); // Kubus tambahan.
+    }
 }
 
 void drawScene(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	//glEnable(GL_LIGHTING);
-	glShadeModel(GL_SMOOTH);
-	GLfloat light_position[] = { 0.0, 10.0, 0.0, 1.0 };
-	GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
-	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-	glEnable(GL_LIGHT0);
+    //glEnable(GL_LIGHTING);
+    glShadeModel(GL_SMOOTH);
+    GLfloat light_position[] = { 0.0, 10.0, 0.0, 1.0 };
+    GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glEnable(GL_LIGHT0);
 
+    glLoadIdentity();
 
-	glLoadIdentity();
+    // Perbarui sudut rotasi.
+    rotationAngle += rotationSpeed;
 
-	// Perbarui sudut rotasi.
-	rotationAngle += rotationSpeed;
+    drawTumpukanRak();
+    drawFrameKanan();
+    drawFrameKiri();
+    drawFrameBlkg();
 
-	drawTumpukanRak();
-	drawFrameKanan();
-	drawFrameKiri();
-	drawFrameBlkg();
+    glDisable(GL_LIGHTING);
 
-	glDisable(GL_LIGHTING);
+    glFlush();
+    glutSwapBuffers();
 
-	glFlush();
-	glutSwapBuffers();
-
-	// Pemicu ulang penggambaran (redisplay) setiap frame.
-	glutPostRedisplay();
+    // Pemicu ulang penggambaran (redisplay) setiap frame.
+    glutPostRedisplay();
 }
 
 // Initialization routine.
 void setup(void)
 {
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glEnable(GL_DEPTH_TEST);
-
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glEnable(GL_DEPTH_TEST);
 }
 
 // OpenGL window reshape routine.
 void resize(int w, int h)
 {
-	glViewport(0, 0, w, h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glFrustum(-5.0, 5.0, -5.0, 5.0, 5.0, 100.0);
-
-	glMatrixMode(GL_MODELVIEW);
-}
-
-void update(int value) {
-	rotationAngle += 1.5f;  // Tingkat rotasi
-	if (rotationAngle > 360) {
-		rotationAngle -= 360;  // Reset sudut rotasi setelah melebihi 360 derajat
-	}
-	glutPostRedisplay();  // Meminta tampilan ulang jendela
-	glutTimerFunc(25, update, 0);  // Atur timer untuk memanggil fungsi update lagi
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-5.0, 5.0, -5.0, 5.0, 5.0, 100.0);
+    glMatrixMode(GL_MODELVIEW);
 }
 
 // Keyboard input processing routine.
 void keyInput(unsigned char key, int x, int y)
 {
-	switch (key)
-	{
-	case 27:
-		exit(0);
-		break;
-	default:
-		break;
-	}
+    switch (key) {
+    case 27:
+        exit(0);
+        break;
+    default:
+        break;
+    }
 }
 
 void mouse(int button, int state, int x, int y) {
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-		prevX = x;
-		prevY = y;
-		mouseRotate = true;
-	}
-	else {
-		mouseRotate = false;
-	}
+    if (button == GLUT_LEFT_BUTTON) {
+        if (state == GLUT_DOWN) {
+            prevX = x;
+            prevY = y;
+            mouseRotate = true;
+        }
+        else if (state == GLUT_UP) {
+            mouseRotate = false;
+        }
+    }
 }
 
 void motion(int x, int y) {
-	if (mouseRotate) {
-		int deltaX = x - prevX;
-		rotationAngle += deltaX;
-		prevX = x;
-		glutPostRedisplay();
-	}
+    if (mouseRotate) {
+        int deltaX = x - prevX;
+        int deltaY = y - prevY;
+        rotationAngle += deltaX;
+        prevX = x;
+        rotationAngle += deltaY;
+        prevY = y;
+        glutPostRedisplay();
+    }
 }
+
 // Main routine.
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-	glutInit(&argc, argv);
+    glutInit(&argc, argv);
 
-	glutInitContextVersion(4, 3);
-	glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+    glutInitContextVersion(4, 3);
+    glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
 
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-	glutInitWindowSize(1000, 1000);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("rakSepatu.cpp");
-	glutDisplayFunc(drawScene);
-	glutReshapeFunc(resize);
-	glutKeyboardFunc(keyInput);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowSize(1000, 1000);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("rakSepatu.cpp");
+    glutDisplayFunc(drawScene);
+    glutReshapeFunc(resize);
+    glutKeyboardFunc(keyInput);
 
-	glewExperimental = GL_TRUE;
-	glewInit();
+    glewExperimental = GL_TRUE;
+    glewInit();
 
-	setup();
+    setup();
 
-	glutMouseFunc(mouse);
-	glutMotionFunc(motion);
-	glutTimerFunc(25, update, 0);
-	//update(0);
-	glutMainLoop();
-	return 0;
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+
+    glutMainLoop();
+
+    return 0;
 }
-
